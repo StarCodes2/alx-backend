d8fe733acbde729d43b895805ea52760ca0d7589
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-20 20:25:40 +0000

    To implement a simple caching system.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Defines class BasicCache that inherits from BaseCaching and impliments
+    a caching system
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ defines a simple caching system that add items to a cache and
+        remove them by key
+    """
+    def __init__(self):
+        """ Initialise an object. """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds a new item to the cache. """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returns an item from the cache that matches a given key. """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -1,2 +1,4 @@
 # 0x01. Caching
 ## Files
+- **base_caching.py**: Holds the base class BaseCaching.
+- **0-basic_cache.py**: Holds the class BasicCache that inherits from BaseCaching and defines a caching system.
