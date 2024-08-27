5c4f7e952a39dfc789a8a8ec2830c0d7f6a48f86
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-27 15:09:45 +0000

    To setup get_locale function with the babel.localeselector decorator.

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Flask app that implement a single route. """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Configuration for available languages and Babel settings. """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Gets the prefered language. """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Return a jinja template """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -3,6 +3,8 @@
 - **templates**: Holds all the jinja template files.
 	- **0-index.html**: Jinja template that simply outputs “Welcome to Holberton” as page title (title) and “Hello world” as header (h1).
 	- **1-index.html**:  Jinja template that simply outputs “Welcome to Holberton” as page title (title) and “Hello world” as header (h1).
+	- **2-index.html**:  Jinja template that simply outputs “Welcome to Holberton” as page title (title) and “Hello world” as header (h1).
 ## Files
 - **0-app.py**: Flask app with one route.
 - **1-app.py**: Flask app with with Babel's deefault locale and timezone setup
+- **2-app.py**: Flask app with the default lacale and timezone setup, and get_locale created with the babel.localeselector decorator.
